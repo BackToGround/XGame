123c9c30998b3b08e1bfe6e350a42ed0bdaafbd3
DESKTOP-FKOTL6F\tachen <tachen@foxmail.com>
2017-09-17 01:01:07 +0800

    帧同步测试

---
+++ Assets/ThirdPlugins/TrueSyncDemo/ReplayPicker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TrueSync;
+
+// attached to each room on join list to do a join if the players hit the button
+public class ReplayPicker : MonoBehaviour {
+
+    public static ReplayRecordInfo replayToLoad;
+
+    public ReplayRecordInfo replayRecord;	
+
+	public void Pick () {
+        replayToLoad = replayRecord;
+        ReplayRecord.replayMode = ReplayMode.LOAD_REPLAY;
+    }   
+
+}
